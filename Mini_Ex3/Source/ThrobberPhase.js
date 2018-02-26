c42eed72717e780ab5f31d005ba29ec8f6df8995
Frederik la Cour <frediklac@gmail.com>
2018-02-26 20:04:44 +0100

    Update ThrobberPhase.js

--- Mini_Ex3/Source/ThrobberPhase.js
+++ Mini_Ex3/Source/ThrobberPhase.js
@@ -11,24 +11,19 @@ var left;
 var right;
 var reverb;
 var filter;
-var vol = 1;
+var vol = 0.8;
 
 function preload() {
   soundFormats('mp3');
-  left = loadSound("89_59_metro.mp3");
-  right = loadSound("89_59_metro.mp3");
-    reverb = new p5.Reverb();
-    lowpass = new p5.BandPass();
+  left = loadSound("buffersize 134.15BPM.mp3");
+  right = loadSound("buffersize 134.15BPM.mp3");
 }
 
 function setup(){
-  //Canvas settings and Framerate
+    //Canvas settings and Framerate
     createCanvas(800,400);
     background(10);
-    frameRate(60);
-  //Initiation of the preloaded clips
-    reverb.process(left, 3, 2);
-      reverb.process(right, 3, 2);
+    frameRate(60);  //Initiation of the preloaded clips
 
     left.loop();
     left.setVolume(vol);
@@ -38,13 +33,13 @@ function setup(){
       right.loop();
       right.setVolume(vol);
       right.pan(1);
-      right.rate(0.985);
+      right.rate(0.995 );
 }
 
 function draw(){
 //drawing the two throbbers
   noStroke();
-  fill(10,10);
+  fill(10,1);
   rect(0, 0, width, height);
   drawThrobber1(32); //left side.
   drawThrobber2(32.985); // right side slightly offset by 0.985.
@@ -61,7 +56,7 @@ var t = 0;
 
 function drawThrobber1(num) { //left
   push();
-  r = (r+4.3985/2);  // r+4.5 = 89.59 BPM
+  r = (left.currentTime()*50);  // the currentTime function enables the use the exact timing from the buffered soundfile
   translate(width/1.5, height/2);
   rotate(radians(r));
   noStroke();
@@ -72,7 +67,7 @@ function drawThrobber1(num) { //left
 
 function drawThrobber2(num) { //right
   push();
-  t =t+4.455/2;
+  t =right.currentTime()*50;
   translate(width/4, height/2);
   rotate(radians(t));
   noStroke();
