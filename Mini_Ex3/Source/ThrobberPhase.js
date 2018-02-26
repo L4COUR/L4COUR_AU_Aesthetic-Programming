45fd10a9e004ec8e7a20fc39e9521938dfc284f2
Frederik la Cour <frediklac@gmail.com>
2018-02-26 15:14:42 +0100

    Update ThrobberPhase.js

--- Mini_Ex3/Source/ThrobberPhase.js
+++ Mini_Ex3/Source/ThrobberPhase.js
@@ -1,13 +1,22 @@
-var song;
-var soong;
+/* This code requires the use of headphones in order to facilitate 
+the full experience of complex phasing. The idea is that the two throbbers each corresponds
+to the timing of the loops in the Left and Right ear cup. one of the loops are slightly faster 
+in terms of completing one full revolution, thus inducing the complex phasing phenomenon.
+
+the Code is also heavily inspired from Steve Reichs early explorations into phasing recordings of 
+bits of spoken phrases repeatedly played over and over again, with the signal split and panned directly
+to the left and right channels.
+*/
+var left;
+var right;
 var reverb;
 var filter;
 var vol = 1;
 
 function preload() {
   soundFormats('mp3');
-  song = loadSound("89_59_metro.mp3");
-  soong = loadSound("89_59_metro.mp3");
+  left = loadSound("89_59_metro.mp3");
+  right = loadSound("89_59_metro.mp3");
     reverb = new p5.Reverb();
     lowpass = new p5.BandPass();
 }
@@ -18,18 +27,18 @@ function setup(){
     background(10);
     frameRate(60);
   //Initiation of the preloaded clips
-    reverb.process(song, 3, 2);
-      reverb.process(soong, 3, 2);
+    reverb.process(left, 3, 2);
+      reverb.process(right, 3, 2);
 
-    song.loop();
-    song.setVolume(vol);
-    song.pan(-1);
-    song.rate(1);
+    left.loop();
+    left.setVolume(vol);
+    left.pan(-1);
+    left.rate(1);
 
-      soong.loop();
-      soong.setVolume(vol);
-      soong.pan(1);
-      soong.rate(0.985);
+      right.loop();
+      right.setVolume(vol);
+      right.pan(1);
+      right.rate(0.985);
 }
 
 function draw(){
@@ -37,8 +46,8 @@ function draw(){
   noStroke();
   fill(10,10);
   rect(0, 0, width, height);
-  drawThrobber1(32);
-  drawThrobber2(32.985); //slightly offsetting one of them by 0.985 in rotation speed
+  drawThrobber1(32); //left side.
+  drawThrobber2(32.985); // right side slightly offset by 0.985.
 
   stroke(255)
   strokeWeight(0.5)
@@ -50,7 +59,7 @@ function draw(){
 var r = 0;
 var t = 0;
 
-function drawThrobber1(num) {
+function drawThrobber1(num) { //left
   push();
   r = (r+4.3985/2);  // r+4.5 = 89.59 BPM
   translate(width/1.5, height/2);
@@ -61,7 +70,7 @@ function drawThrobber1(num) {
   pop();
 }
 
-function drawThrobber2(num) {
+function drawThrobber2(num) { //right
   push();
   t =t+4.455/2;
   translate(width/4, height/2);
