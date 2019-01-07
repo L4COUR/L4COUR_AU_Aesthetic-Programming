76aefdf644a6977e72d07f501c950a3fbcfe3601
Frederik la Cour <frediklac@gmail.com>
2019-01-07 02:50:27 +0100

    fixing some patch bugz ;)

--- Max_patch animation/Source/sketch.js
+++ Max_patch animation/Source/sketch.js
@@ -4,8 +4,11 @@
 // connecting objects to objects, and using it as a button for controlling something.
 
 //variables for the MaxObj
-let maxobject1;
-let maxobject2;
+//let maxobject1;
+//let maxobject2;
+
+var maxobject = [1,2];
+
 var isOverOutput;
 var isOverInput;
 var overObj = false;
@@ -19,27 +22,27 @@ var dragY;
 var moveX;
 var moveY;
 
-var x1;
-var y1;
+var x1 = maxobject[1].ex;
+var y1 = maxobject[1].ey;
 var x2;
 var y2;
 
 function setup() {
   background(180);
   createCanvas(windowWidth,windowHeight);
-  maxobject1 = new MaxObj("Home~",200,150,180,50,15,15);
-  maxobject2 = new MaxObj("Works",250,350,200,50,15,15);
+  background(180);
+  maxobject[1] = new MaxObj("Home~",200,150,180,50,15,15);
+  maxobject[2] = new MaxObj("Works",250,350,200,50,15,15);
+  maxobject[3] = new MaxObj("About",350,250,140,50,15,15);
 }
 
 function mousePressed() {
   //maxobject1.io();
   //maxobject2.io();
 
-  //if (isOverOutput = true) {
+  if (x1 > 0, x2 > 0, y1 > 0, y2 > 0) {
     patch_chord.push(new Patcher(x1,y1,x2,y2));
-  //}
-
-
+  }
   //if (overObj == true){
   //  patch_chord.push(new Patcher(maxobject1.ex,maxobject1.ey,mouseX,mouseY));
   //}
@@ -51,17 +54,41 @@ function draw() {
   background(180);
   //patch_chord = new Patcher(maxobject1.ex,maxobject1.ey,mouseX,mouseY);
 
-  maxobject1.show();
-  maxobject2.show();
+  maxobject[1].show();
+  maxobject[2].show();
+  maxobject[3].show();
   //patch_chord.show();
 
   for (var i = 0; i < patch_chord.length; i++) {
    patch_chord[i].show();
+   patch_chord[i].move();
+
+//  if(x2 > 0){
+//     console.log("Reset line parameters")
+//     set(x1 = undefined, y1 = undefined);
+//     set(x2 = undefined, y2 = undefined);
+//   }
    //patch_chord[i].move();
   }
+  stroke(88)
+  strokeWeight(8);
+  line(x1,y1,x2,y2)
+  stroke(150)
+  strokeWeight(6);
+  line(x1,y1,x2,y2)
 
 }
 
+function mouseReleased(move) {
+  //set(patch_chord[0].x1,patch_chord[0].x2);
+  //console.log("Hey")
+//  if(y2 > undefined){
+//    console.log("Hey")
+//    set(x1 = undefined, y1 = undefined);
+//    set(x2 = undefined, y2 = undefined);
+//  }
+//patch_chord[i].move();
+}
 
 class MaxObj {
   constructor(name,x,y,w,h,udr,indr) {
@@ -118,14 +145,18 @@ class MaxObj {
     if(distance1 < this.udr && mousePressed)
     {
       isOverOutput = true;
-      console.log("Output", this.name);
+      //console.log("Output", this.name);
       set(x1 = this.ex,y1 = this.ey);
       //patch_chord.push(new Patcher(this.ex,this.ey,maxobject2.ex,maxobject2.y));
     } else {
       isOverOutput = false;
+      //set(x2 = undefined, y2 = undefined);
+      //set(x1 = undefined, y2 = undefined);
       //console.log("out", this.name);
-
     }
+
+
+
 
     // output patchpoint
     ellipseMode(CENTER);
@@ -146,16 +177,20 @@ class MaxObj {
     // Object input
     var distance2 = dist(mouseX, mouseY, this.ex, this.y);
     // if the distance is less than the circle's radius
-    if(distance2 < this.indr && mouseIsPressed)
+    if(distance2 < this.indr && mousePressed)
     {
       isOverInput = true;
       console.log("Input", this.name);
       set(x2 = this.ex, y2 = this.y);
+      //set(x1 = undefined, y1 = undefined);
     //  patch_chord.push(new Patcher(this.ex,this.ey,maxobject2.ex,maxobject2.y));
-    } else {
-      isOverInput = false;
-
-    }
+  } else {
+    isOverInput = false
+    //console.log("away from Input");
+    //set(x2 = undefined, y2 = undefined);
+    //set(x1 = undefined, y2 = undefined);
+    //console.log("out", this.name);
+  }
 
     // Input patchpoint
     ellipseMode(CENTER);
@@ -174,6 +209,8 @@ class MaxObj {
     ellipse(this.ex, this.y, this.indr, this.indr);
 
   }
+
+
 }
 
 class Patcher {
@@ -191,5 +228,16 @@ class Patcher {
     stroke(150)
     strokeWeight(6);
     line(this.x1,this.y1,this.x2,this.y2)
+  }
+
+  move() {
+    if (this.x1 == this.x2) {
+      noStroke();
+      console.log("RESET");
+      this.x1 = undefined;
+      this.x2 = undefined;
+      this.y1 = undefined;
+      this.y2 = undefined;
+    }
   }
 }
